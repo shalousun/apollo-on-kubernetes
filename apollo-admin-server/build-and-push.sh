08d16e367f9f4387e861a504b9476600425ce176
oppofind <836575280@qq.com>
2018-11-17 00:13:05 +0800

    Initial commit apollo v1.1.2

---
+++ apollo-admin-server/build-and-push.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# apollo version
+VERSION=v1.0.0
+
+APP_NAME=apollo-admin-server
+
+DOCKER_REGISTRY=docker-server.com
+
+HARBOR_PROJECT=library
+
+HARBOR_USER=admin
+
+HARBOR_PASSWORD=Harbor12345
+
+MYIMAGE=$APP_NAME:$VERSION
+# =========================build images========================================
+docker build -t $APP_NAME:$VERSION .
+
+# ==========================push image to registry========================
+# uncomment if you need push
+docker login ${DOCKER_REGISTRY} -u $HARBOR_USER -p $HARBOR_PASSWORD
+echo "INFO：Starting push image of ${MYIMAGE} to docker registry ${DOCKER_REGISTRY}"
+docker tag ${MYIMAGE}  ${DOCKER_REGISTRY}/$HARBOR_PROJECT/${MYIMAGE}
+docker push ${DOCKER_REGISTRY}/$HARBOR_PROJECT/${MYIMAGE}
